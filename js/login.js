f1d1f0d3d5fd43dbd5c02551b758921492a1788a
RohiniG-12 <garudkarrohini0170@gmail.com>
2024-06-16 17:32:58 +0530

    js file and other changes complete

---
+++ js/login.js
@@ -0,0 +1,21 @@
+function submit(){
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+     if(email.value == "")
+    {
+        alert("Please Enter Your Email-Id")
+    }
+    else 
+    if(password.value == "")
+    {
+        alert("Please Enter Your Password")
+    }
+    else 
+     alert("Login successful");
+
+   
+}
+function forgot()
+{
+  alert("Login with Gmail");
+}
