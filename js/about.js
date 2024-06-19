6bd08a9b0c2fba6a5a5cc00528dbe8087dd91876
RohiniG-12 <garudkarrohini0170@gmail.com>
2024-06-19 15:31:55 +0530

    home page js completed

---
+++ js/about.js
@@ -0,0 +1,15 @@
+function change()
+{
+ let image = document.getElementById('img1');
+ image.src = '../images/Desktop_5.png';
+}
+function change1()
+{
+ let image = document.getElementById('img2');
+ image.src = '../images/Desktop_6.png';
+}
+function change2()
+{
+ let image = document.getElementById('img3');
+ image.src = '../images/Desktop_7.png';
+}
---
+++ js/index.js
@@ -0,0 +1,4 @@
+function change(){
+    let slider = document.getElementById('body-img');
+    slider.src = '../images/home2.webp';
+}
