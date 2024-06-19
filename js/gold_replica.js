9f8d8f986ff94ad5b61b1bf7dedee3ef44fab40e
RohiniG-12 <garudkarrohini0170@gmail.com>
2024-06-19 17:24:13 +0530

    js applied to all the pages

---
+++ js/gold_replica.js
@@ -0,0 +1,189 @@
+function incre(){
+    const quantityText = document.getElementById("quantity-text");
+    const total = document.getElementById("total");
+    const showqauntity = document.getElementById("showqauntity")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity<10){
+    quantityText.innerText = ++quantity;
+    }
+    else{
+        alert('You cant order more than 10 items')
+    }
+    total.innerText = `Total:${quantity*123999}/-` ;
+    showqauntity.innerText = `${quantity} x 1,23,999`;
+ }
+
+ function decre(){
+    const quantityText = document.getElementById("quantity-text");
+    const total = document.getElementById("total");
+    const showqauntity= document.getElementById("showqauntity")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity>1){
+    quantityText.innerText = --quantity;
+    }
+    else{
+        alert('You cant order less than 1 items')
+    }
+    total.innerText = ` Total:${quantity*123999}/-`;
+    showqauntity.innerText = `${quantity} x 1,23,999`;
+   
+ }
+
+//gold kolhapuri saj
+function incre1(){
+    const quantityText = document.getElementById("quantity-text1");
+    const total = document.getElementById("total1");
+    const showqauntity = document.getElementById("showqauntity1")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity<10){
+    quantityText.innerText = ++quantity;
+    }
+    else{
+        alert('You cant order more than 10 items')
+    }
+    total1.innerText = `Total:${quantity*123999}/-` ;
+    showqauntity1.innerText = `${quantity} x 1,23,999`;
+ }
+
+ function decre1(){
+    const quantityText = document.getElementById("quantity-text1");
+    const total = document.getElementById("total1");
+    const showqauntity = document.getElementById("showqauntity1")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity>1){
+    quantityText.innerText = --quantity;
+    }
+    else{
+        alert('You cant order less than 1 items')
+    }
+    total1.innerText = ` Total:${quantity*123999}/-`;
+    showqauntity1.innerText = `${quantity} x 1,23,999`;
+   
+ }
+
+ //
+ function incre2(){
+    const quantityText = document.getElementById("quantity-text2");
+    const total = document.getElementById("total2");
+    const showqauntity = document.getElementById("showqauntity2")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity<10){
+    quantityText.innerText = ++quantity;
+    }
+    else{
+        alert('You cant order more than 10 items')
+    }
+    total2.innerText = `Total:${quantity*123999}/-` ;
+    showqauntity2.innerText = `${quantity} x 1,23,999`;
+ }
+
+ function decre2(){
+    const quantityText = document.getElementById("quantity-text2");
+    const total = document.getElementById("total2");
+    const showqauntity = document.getElementById("showqauntity2")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity>1){
+    quantityText.innerText = --quantity;
+    }
+    else{
+        alert('You cant order less than 1 items')
+    }
+    total2.innerText = ` Total:${quantity*123999}/-`;
+    showqauntity2.innerText = `${quantity} x 1,23,999`;
+   
+ }
+ //
+ function incre3(){
+    const quantityText = document.getElementById("quantity-text3");
+    const total = document.getElementById("total3");
+    const showqauntity = document.getElementById("showqauntity3")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity<10){
+    quantityText.innerText = ++quantity;
+    }
+    else{
+        alert('You cant order more than 10 items')
+    }
+    total1.innerText = `Total:${quantity*123999}/-` ;
+    showqauntity1.innerText = `${quantity} x 1,23,999`;
+ }
+
+ function decre3(){
+    const quantityText = document.getElementById("quantity-text3");
+    const total = document.getElementById("total3");
+    const showqauntity = document.getElementById("showqauntity3")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity>1){
+    quantityText.innerText = --quantity;
+    }
+    else{
+        alert('You cant order less than 1 items')
+    }
+    total3.innerText = ` Total:${quantity*123999}/-`;
+    showqauntity3.innerText = `${quantity} x 1,23,999`;
+   
+ }
+
+ //
+ function incre4(){
+    const quantityText = document.getElementById("quantity-text4");
+    const total = document.getElementById("total4");
+    const showqauntity = document.getElementById("showqauntity4")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity<10){
+    quantityText.innerText = ++quantity;
+    }
+    else{
+        alert('You cant order more than 10 items')
+    }
+    total4.innerText = `Total:${quantity*123999}/-` ;
+    showqauntity4.innerText = `${quantity} x 1,23,999`;
+ }
+
+ function decre4(){
+    const quantityText = document.getElementById("quantity-text4");
+    const total = document.getElementById("total4");
+    const showqauntity = document.getElementById("showqauntity4")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity>1){
+    quantityText.innerText = --quantity;
+    }
+    else{
+        alert('You cant order less than 1 items')
+    }
+    total4.innerText = ` Total:${quantity*123999}/-`;
+    showqauntity4.innerText = `${quantity} x 1,23,999`;
+   
+ }
+
+ //
+ function incre5(){
+    const quantityText = document.getElementById("quantity-text5");
+    const total = document.getElementById("total5");
+    const showqauntity = document.getElementById("showqauntity5")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity<10){
+    quantityText.innerText = ++quantity;
+    }
+    else{
+        alert('You cant order more than 10 items')
+    }
+    total5.innerText = `Total:${quantity*123999}/-` ;
+    showqauntity5.innerText = `${quantity} x 1,23,999`;
+ }
+
+ function decre5(){
+    const quantityText = document.getElementById("quantity-text5");
+    const total = document.getElementById("total5");
+    const showqauntity = document.getElementById("showqauntity5")
+    let quantity = parseInt(quantityText.innerText);
+    if(quantity>1){
+    quantityText.innerText = --quantity;
+    }
+    else{
+        alert('You cant order less than 1 items')
+    }
+    total5.innerText = ` Total:${quantity*123999}/-`;
+    showqauntity5.innerText = `${quantity} x 1,23,999`;
+   
+ }
